628f6ec5f8e8f5022ebd3ebc8d97b9f5d6158778
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2013-12-15 16:43:22 -0500

    Remote control, with Flash info service.

--- apps/epaper/epaper.c
+++ apps/epaper/epaper.c
@@ -96,24 +96,93 @@ void flash_info(uint8_t *maufacturer, uint16_t *device) {
 
 /*** /FLASH */
 
-void usbToRadioService()
+#define LED_YELLOW(x) do { setDigitalOutput(PIN_PWR, x); } while(0)
+
+void updateLeds()
 {
-    while(usbComRxAvailable() && radioComTxAvailable())
+    static BIT dimYellowLed = 0;
+    static uint16 lastRadioActivityTime;
+    uint16 now;
+
+    usbShowStatusWithGreenLed();
+
+    now = (uint16)getMs();
+
+    if (!radioLinkConnected())
     {
-        radioComTxSendByte(usbComRxReceiveByte());
+        // We have not connected to another device wirelessly yet, so do a
+        // 50% blink with a period of 1024 ms.
+        LED_YELLOW(now & 0x200 ? 1 : 0);
+    }
+    else
+    {
+        // We have connected.
+
+        if ((now & 0x3FF) <= 20)
+        {
+            // Do a heartbeat every 1024ms for 21ms.
+            LED_YELLOW(1);
+        }
+        else if (dimYellowLed)
+        {
+            static uint8 DATA count;
+            count++;
+            LED_YELLOW((count & 0x7)==0);
+        }
+        else
+        {
+            LED_YELLOW(0);
+        }
     }
 
-    while(radioComRxAvailable() && usbComTxAvailable())
+    if (radioLinkActivityOccurred)
     {
-        usbComTxSendByte(radioComRxReceiveByte());
+        radioLinkActivityOccurred = 0;
+        dimYellowLed ^= 1;
+        //dimYellowLed = 1;
+        lastRadioActivityTime = now;
+    }
+
+    if ((uint16)(now - lastRadioActivityTime) > 32)
+    {
+        dimYellowLed = 0;
+    }
+}
+
+
+void cmdFlashInfo() {
+    uint8_t flash_manufacturer;
+    uint16_t flash_device;
+    flash_info(&flash_manufacturer, &flash_device);
+    flash_info(&flash_manufacturer, &flash_device);
+    if (flash_manufacturer == FLASH_MFG && flash_device == FLASH_ID) {
+        printf("Flash good: ");
+    } else {
+        printf("Flash what: ");
+    }
+    printf("%x, %hx\n", flash_manufacturer, flash_device);
+}
+
+void remoteControlService() {
+    if (!radioComRxAvailable()) return;
+
+    switch(radioComRxReceiveByte()) {
+    case 'f': cmdFlashInfo(); break;
+    default: printf("? ");
     }
 }
 
 // Called by printf.
 void putchar(char c)
 {
-    while (!usbComTxAvailable()) usbComService();
-    usbComTxSendByte(c);
+    while (!radioComTxAvailable()) radioComTxService();
+    if (usbComTxAvailable()) usbComTxSendByte(c);
+    if (radioComTxAvailable()) radioComTxSendByte(c);
+}
+
+// Called by scanf.
+char getchar() {
+    return radioComRxReceiveByte();
 }
 
 uint8 XDATA spiTxBuffer[10];
@@ -121,11 +190,6 @@ uint8 XDATA spiRxBuffer[10];
 
 void main()
 {
-    uint8_t flash_manufacturer;
-    uint16_t flash_device;
-    uint16_t timeOfLastMsg = 0;
-
-
     systemInit();
     spi0MasterInit();
     spi0MasterSetFrequency(3000000);
@@ -144,44 +208,17 @@ void main()
 
     usbInit();
 
-    radioComRxEnforceOrdering = 1;
+    // Don't "enforce ordering" because we're not going to use the "control signals".
+    radioComRxEnforceOrdering = 0;
     radioComInit();
-
-    //delayMs(5000);
-//
-    flash_info(&flash_manufacturer, &flash_device);
-    flash_info(&flash_manufacturer, &flash_device);
-    if (flash_manufacturer == FLASH_MFG && flash_device == FLASH_ID) {
-        printf("Flash good.\n");
-    } else {
-        printf("Flash what?\n");
-    }
-    printf("%x, %hx\n", flash_manufacturer, flash_device);
-
 
     while(1)
     {
-        uint16 now;
         boardService();
         radioComTxService();
         usbComService();
+        updateLeds();
 
-        usbShowStatusWithGreenLed();
-
-        now = (uint16)getMs();
-        if ((now & 0x3FF) <= 20) {
-            setDigitalOutput(PIN_PWR, 1);
-        } else {
-            setDigitalOutput(PIN_PWR, 0);
-        }
-        if (now - timeOfLastMsg > 1000) {
-            printf("-\n");
-            timeOfLastMsg = now;
-
-        }
-
-        //usbToRadioService();
-        //while (spi0MasterBusy());
-        //spi0MasterTransfer(spiTxBuffer, spiRxBuffer, 2);
+        remoteControlService();
     }
 }
