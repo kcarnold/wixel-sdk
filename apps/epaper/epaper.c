a607bd4caf2469454af4956a5fc63469576fceb8
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2013-12-15 23:56:32 -0500

    Stop abusing the output pins for LED status.
    As an added bonus, SPI now works more reliably.

--- apps/epaper/epaper.c
+++ apps/epaper/epaper.c
@@ -14,60 +14,6 @@
 #include "flash.h"
 #include "epd.h"
 
-#undef LED_YELLOW
-#define LED_YELLOW(x) do { setDigitalOutput(PIN_PWR, x); } while(0)
-
-void updateLeds()
-{
-    static BIT dimYellowLed = 0;
-    static uint16 lastRadioActivityTime;
-    uint16 now;
-
-    usbShowStatusWithGreenLed();
-
-    now = (uint16)getMs();
-
-    if (!radioLinkConnected())
-    {
-        // We have not connected to another device wirelessly yet, so do a
-        // 50% blink with a period of 1024 ms.
-        LED_YELLOW(now & 0x200 ? 1 : 0);
-    }
-    else
-    {
-        // We have connected.
-
-        if ((now & 0x3FF) <= 20)
-        {
-            // Do a heartbeat every 1024ms for 21ms.
-            LED_YELLOW(1);
-        }
-        else if (dimYellowLed)
-        {
-            static uint8 DATA count;
-            count++;
-            LED_YELLOW((count & 0x7)==0);
-        }
-        else
-        {
-            LED_YELLOW(0);
-        }
-    }
-
-    if (radioLinkActivityOccurred)
-    {
-        radioLinkActivityOccurred = 0;
-        dimYellowLed ^= 1;
-        //dimYellowLed = 1;
-        lastRadioActivityTime = now;
-    }
-
-    if ((uint16)(now - lastRadioActivityTime) > 32)
-    {
-        dimYellowLed = 0;
-    }
-}
-
 
 void cmdFlashInfo() {
     uint8_t flash_manufacturer;
@@ -211,7 +157,6 @@ void main()
         boardService();
         radioComTxService();
         usbComService();
-        updateLeds();
 
         remoteControlService();
     }
