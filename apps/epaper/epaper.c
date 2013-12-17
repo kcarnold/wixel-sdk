b7fdb86830ba08e1b9546e3573c135fbc34fb7f1
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2013-12-17 01:01:54 -0500

    Need to write-enable before every write operation, apparently.

--- apps/epaper/epaper.c
+++ apps/epaper/epaper.c
@@ -112,6 +112,7 @@ void cmdUpload() {
     flash_write_enable();
     flash_sector_erase(address);
     delayMs(1);
+    flash_write_enable();
     flash_sector_erase(address + 1);
     putchar('>'); // Go!
     while (bytes) {
